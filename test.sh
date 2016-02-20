ec7c0187d52a8627be7113af55c216f0a46935bd
Anis GADHGADHI <agadhgad@e3r6p4.42.fr>
2016-02-20 21:21:04 +0100

    fusion fnctn error et valid

--- test.sh
+++ test.sh
@@ -3,96 +3,81 @@ MYPATH="$1"
 command -v $MYPATH/fillit >/dev/null 2>&1 || { echo "Don't find fillit!\nNOTE: \033[0;31m0 / 42\033[0m"; exit 1; }
 echo "fillit_checker by agadhgad"
 LOCALPATH="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
+# nb of total valid test
 nb=0
-function test_error()
+
+# $1: i
+function error_put()
 {
-
-i=0
-echo "--- begin of invalid test ---"
-echo "\033[0;31mWARNING: stdout != stderr\033[0m"
-while [ "$i" != 20 ]
-do
-	echo "invalid test $i : \c"
-	var=$((${MYPATH}/./fillit ${LOCALPATH}/error_file/error_$i) 2>&-)
-	if [ "$var" != "error" ]
-	then
-		echo "\033[0;31mfail :/\033[0m"
-		echo "for the file error_$i (cat -e):"
-		cat -e ${LOCALPATH}/error_file/error_$i
-		echo "your output is:"
-		echo "$var"
-	else
-		echo "\033[0;32mok\033[0m"
-		nb=$((nb+1))
-	fi
-	i=$((i+1))
-done
-echo "--- end of invalid test ---"
-if [ "$nb" != 20 ]
-then
-	echo "\033[0;31minvalid test failed, $nb / 20\033[0m"
-else
-	echo "\033[0;32minvalid test succeed, $nb / 20\033[0m"
-fi
-
+	echo "for the file error_$1 (cat -e):"
+	cat -e ${LOCALPATH}/error_file/error_$1
+	echo "your output is:"
 }
-nb2=0
-function test_valid()
+function valid_put()
 {
+	echo "WHAT IS EXPECTED:"
+	cat "${LOCALPATH}/correct_compare/output_valid_$1"
+	echo "YOUR OUTPUT:"
+}
 
 i=0
-echo "___ begin of valid test ___"
-while [ "$i" != 20 ]
-do
-	echo "valid test $i : \c"
-	var=$(${MYPATH}/./fillit ${LOCALPATH}/correct_file/valid_$i 2>&-)
-	var2=$(cat "${LOCALPATH}/correct_compare/output_valid_$i")
-	if [ "$var" != "$var2" ]
+# $1: type test $2: pathfolder&name $3: number of test arg $4: var2 $5: 1 if var2 take i for arg, else 0 $6 fnctn error
+function test()
+{
+	test_nb=0
+	i=0
+	echo "--- begin of $1 test ---"
+	while [ "$i" != $3 ]
+	do
+		echo "$1 test $i : \c"
+		var=$((${MYPATH}/./fillit ${LOCALPATH}/$2$i) 2>&-)
+		if [ "$5" == 1 ]
+		then
+			var2=$($4$i)
+		else
+			var2=$($4)
+		fi
+		# echo "$var2"
+		if [ "$var" != "$var2" ]
+		then
+			echo "\033[0;31mfail :/\033[0m"
+			$6 $i
+			echo "$var"
+		else
+			echo "\033[0;32mok\033[0m"
+			test_nb=$((test_nb+1))
+		fi
+		i=$((i+1))
+	done
+	echo "--- end of $1 test ---"
+	if [ "$test_nb" != $3 ]
 	then
-		echo "\033[0;31mfail :/\033[0m"
-		echo "WHAT IS EXPECTED:"
-		echo "$var2"
-		echo "YOUR OUPUT:"
-		echo "$var"
+		echo "\033[0;31m$1 test failed, $test_nb / $3\033[0m"
 	else
-		echo "\033[0;32mok\033[0m"
-		nb2=$((nb2+1))
+		echo "\033[0;32m$1 test succeed, $test_nb / $3\033[0m"
 	fi
-	i=$((i+1))
-done
-echo "___ end of valid test ___"
-if [ "$nb2" = 20 ]
-then
-	echo "\033[0;32mvalid test succeed $nb2 / 20\033[0m"
-else
-	echo "\033[0;31mvalid test failed, $nb2 / 20\033[0m"
-fi
-
+	nb=$((nb+test_nb))
 }
 
 function bonus()
 {
-
-if [ "$nb" = 40 ]
-then
-
-	echo "\033[0;34mBONUS : \033[0m"
-	time ${MYPATH}/./fillit ${LOCALPATH}/correct_file/valid_20
-	time ${MYPATH}/./fillit ${LOCALPATH}/correct_file/valid_21
-	echo "NOTE: \033[0;32m42/42\033[0m"
-else
-	echo "NOTE: \033[0;31m$nb / 40\033[0m"
-fi
-
+	if [ "$nb" = 40 ]
+	then
+		echo "\033[0;34mBONUS : \033[0m"
+		time ${MYPATH}/./fillit ${LOCALPATH}/correct_file/valid_20
+		time ${MYPATH}/./fillit ${LOCALPATH}/correct_file/valid_21
+		echo "NOTE: \033[0;32m42/42\033[0m"
+	else
+		echo "NOTE: \033[0;31m$nb / 40\033[0m"
+	fi
 }
 
 function main()
 {
-
-test_error
-test_valid
-(( nb += nb2))
-bonus
+	test invalid "error_file/error_" 20 "echo "error"" 0 error_put
+	test valid "correct_file/valid_" 20 "cat ${LOCALPATH}/correct_compare/output_valid_" 1 valid_put
+	bonus
 }
 
 main
